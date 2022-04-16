069940b8d7f1e1f9873bcfadcb0b7b2803efff23
Swayam-555 <radheshyam.panigrahi@gmail.com>
2022-04-16 20:55:49 +0530

    STACK NAVIGATION

---
+++ screens/ISSLocation.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { View, Text } from 'react-native';
+
+export default class ISSLocation extends React.Component {
+    render() {
+        return (
+            <View>
+                <Text>
+                    ISS LOCATOR...
+                </Text>
+            </View>
+        )
+    }
+}
---
+++ screens/homeScreen.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import { View, Text } from 'react-native';
+export default class HomeScreen extends React.Component {
+    render() {
+        return (
+            <View>
+                <Text>
+                    HOMESCREEN OVER HERE!
+                </Text>
+            </View>
+        )
+    }
+}
